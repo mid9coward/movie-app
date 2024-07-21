740aef4adcb6195d75d16be690216d39090a950f
[mid9coward] <[hieuvu0422@gmail.com]>
2024-07-21 22:39:13 +0700

    updated

--- src/components/Category.js
+++ src/components/Category.js
@@ -20,6 +20,7 @@ const yearList = [
   { id: 2021, label: "2021" },
   { id: 2022, label: "2022" },
   { id: 2023, label: "2023" },
+  { id: 2024, label: "2024" },
 ];
 
 export default function Category() {
